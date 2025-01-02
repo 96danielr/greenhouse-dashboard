e72f8156f8734d35a31eaf02cd9ac0a78faaca66
Daniel Rodriguez <drodriguez@runmyprocess.com>
2025-01-02 08:51:55 -0500

    deploy 1.0

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import theme from './styles/theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Resetea los estilos predeterminados */}
+      <CssBaseline /> 
       <App />
     </ThemeProvider>
   </React.StrictMode>
